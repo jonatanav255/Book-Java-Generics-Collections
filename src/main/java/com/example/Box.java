60c8d85e265271c21e52034ce0f4e0d26e40452b
Jonatan <jonatanav255@hotmail.com>
2025-11-24 23:02:06 -0600

    adding box to practice generics

---
+++ src/main/java/com/example/Box.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Box<T> {
+    // 1. Add a private field to store the item
+    // 2. Create a constructor that takes an item
+    // 3. Create a method to get the item
+    // 4. Create a method to set a new item
+    // 5. Create a method that checks if box is empty
+    // 6. Override toString() to display the item            
+}
