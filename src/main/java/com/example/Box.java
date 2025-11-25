5ec7ce2126d0a7e4e1470a45c85395f60afc0f1e
Jonatan <jonatanav255@hotmail.com>
2025-11-24 23:08:26 -0600

    adding, value, constructor, method

--- src/main/java/com/example/Box.java
+++ src/main/java/com/example/Box.java
@@ -1,8 +1,20 @@
 package com.example;
 
 public class Box<T> {
+
     // 1. Add a private field to store the item
+    private T value;
+
     // 2. Create a constructor that takes an item
+    public Box(T value) {
+        this.value = value;
+
+    }
+
+    public T getBoxItem() {
+        return this.value;
+
+    }
     // 3. Create a method to get the item
     // 4. Create a method to set a new item
     // 5. Create a method that checks if box is empty
